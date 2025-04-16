a049ef01300942a378bf18ffb9dbc637104a7d4b
Beatriz Buava <beatriznfb28@gmail.com>
2025-04-15 21:59:59 -0300

    update tailwind herosectiotext

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -12,46 +12,45 @@ export default function NavBar() {
     { name: "Sobre mim", href: "/aboutsection" },
     { name: "Projetos", href: "/projectsection" },
     { name: "Contato", href: "/emailsection" },
-    
-  ];
-      
-  
-  return (
-    <nav className="fixed w-full z-10 bg-[#759EB8] !font-noticia bg-opacity-100 "
-    style={{ backgroundImage: `url('/assets/texture.png')`, backgroundSize: "fit", backgroundPosition: "center" }}>
-    <div className="flex justify-start  px-4 pt-2 "
-    
-    >
-      {tabs.map((tab) => {
-        const isActive = pathname === tab.href;
-        const baseStyle =
-          "px-5 py-2 text-sm  md:text-base rounded-t-lg  w-50 transition-colors";
-        const activeStyle = " !text-[#FCF6E4] z-10";
-        const tabColor =
-          tab.name === "Home"
-            ? "bg-[#759EB8] !text-[#FCF6E4] hover:bg-green-700 "
-            : tab.name === "Sobre mim"
-            ? "bg-[#03441F] !text-[#FCF6E4] hover:bg-green-700 "
-            : tab.name === "Projetos"
-            ? "bg-[#0A285A] !text-[#FCF6E4] hover:bg-blue-700 "
-            : tab.name === "Contato"
-            ? "bg-[#051116] !text-[#FCF6E4] hover:bg-red-700 "
-            : "bg-[#4F3824] !text-[#FCF6E4] hover:bg-neutral-700 ";
 
-        return (
-          <Link
-            key={tab.name}
-            href={tab.href}
-            className={`${baseStyle} ${tabColor} ${isActive ? activeStyle : ""}`}
-            style={{ backgroundImage: `url('/assets/texture.png')`, backgroundSize: "cover", backgroundPosition: "center" }}
-          >
-            {tab.name}
-          </Link>
-        );
-      })}
-    </div>
-  </nav>
-    );
-  };
-                                    
-                                   
+  ];
+
+
+  return (
+    <nav className="fixed w-full z-10 bg-[#FFFAED] !font-noticia bg-opacity-100 "
+    >
+      <div className="flex justify-end px-4 pt-2 "
+
+      >
+        {tabs.map((tab) => {
+          const isActive = pathname === tab.href;
+          const baseStyle =
+            "px-5 py-2 text-sm  md:text-base rounded-t-lg  w-50 transition-colors";
+          const activeStyle = " !text-[#FCF6E4] z-10";
+          const tabColor =
+            tab.name === "Home"
+              ? "bg-[#7897A6] !text-[#FCF6E4] s border border-white border-b-0 hover:bg-[#436E84] "
+              : tab.name === "Sobre mim"
+                ? "bg-[#86AC6F] !text-[#FCF6E4] border border-white border-b-0 hover:bg-[#6D9158] "
+                : tab.name === "Projetos"
+                  ? "bg-[#BE7C4D] !text-[#FCF6E4] border border-white border-b-0 hover:bg-[#A7693C] "
+                  : tab.name === "Contato"
+                    ? "bg-[#A30000] !text-[#FCF6E4] border border-white border-b-0 hover:bg-[#950000] "
+                    : "bg-[#4F3824] !text-[#FCF6E4] hover:bg-neutral-700 ";
+
+          return (
+            <Link
+              key={tab.name}
+              href={tab.href}
+              className={`${baseStyle} ${tabColor} ${isActive ? activeStyle : ""}`}
+
+            >
+              {tab.name}
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
+};
+
--- src/app/components/band/HeroSectionText.jsx
+++ src/app/components/band/HeroSectionText.jsx
@@ -2,11 +2,11 @@
 import { TypeAnimation } from "react-type-animation";
 
 export function Underlay() {
-    return (
-      <section >
-      <div className="responsive-wrapper absolute bg-[#759EB8] grid grid-cols-1 sm:grid-cols-12 "
-       style={{ backgroundImage: `url('/assets/texture.png')`, backgroundSize: "fit", backgroundPosition: "center" }} >
-        <div className="col-span-7  text-left sm:w-3/4 lg:w-2/3">
+  return (
+    <section >
+      <div className="responsive-wrapper absolute bg-[#7897A6] grid grid-cols-1 sm:grid-cols-12 "
+      >
+        <div className="col-span-7 bg-amber-50 p-5  border-[#323232] border-2 shadow-[4px_4px_0_0_#323232] rounded-sm text-left sm:w-3/4 lg:w-2/3">
           <h1 className="text-[#051116] mb-4   sm:text-5xl lg:text-6xl !font-noticia">
             <span className="text-transparent bg-clip-text bg-[#1E1E1E] text-7xl">
               Olá! Seja bem-vindo.{" "}
@@ -14,31 +14,31 @@ export function Underlay() {
             </span>
             <br />
             <TypeAnimation
-            splitter={(str) => str.split(/(?= )/)} 
-            style={{
-              
-              height: 'auto',
-              width: '800px',
-              display: 'block',
-              fontSize: '2rem',
-              lineHeight: '1.0',
-              marginTop: '1rem',
-              minHeight: '200px',}}
-             
-            sequence={[`Eu sou a Beatriz! Que bom que voce chegou aqui. Por favor, leia com cuidado todas as informações apresentadas aqui.`, 1000,]}
-              
-              
+              splitter={(str) => str.split(/(?= )/)}
+              style={{
+
+                height: 'auto',
+                width: '800px',
+                display: 'block',
+                fontSize: '2rem',
+                lineHeight: '1.0',
+                marginTop: '1rem',
+                minHeight: '200px',
+              }}
+
+              sequence={[`Eu sou a Beatriz! Que bom que voce chegou aqui. Por favor, leia com cuidado todas as informações apresentadas aqui.`, 1000,]}
+
+
               speed={10}
               repeat={0}
             />
           </h1>
-          
+
         </div>
-        
-        </div>
-      </section>
-    )
-  }
-  
-  
-  
+
+      </div>
+    </section>
+  )
+}
+
+
